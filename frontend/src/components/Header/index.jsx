844a96299d112afb6abedbe2c68d5c0a91be6bc6
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-10-07 17:17:15 -0300

    atualização

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+
+    return (
+        
+        <header className="bg-primary mb-5">
+            <div class="container d-flex justify-content-center align-items-center gap-2 py-2">
+                <img src="https://images.mlssoccer.com/image/upload/v1665849438/assets/logos/MLS-Crest-FFF-480px_tmwlkh.png" alt="" />
+                <h2 className="text-white">MLS Simulator</h2>
+            </div>
+        </header>
+    )
+}
