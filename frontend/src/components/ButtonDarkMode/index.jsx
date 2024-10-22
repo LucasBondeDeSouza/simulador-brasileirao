24abaf519f0c051de47315fd5f339f424b3d6ea7
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-10-22 13:28:20 -0300

    atualização

--- frontend/src/components/ButtonDarkMode/index.jsx
+++ frontend/src/components/ButtonDarkMode/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default ({ toggleDarkMode, darkMode }) => {
 
     return (
         <button className="btn btn-primary rounded-circle d-flex justify-content-center align-items-center position-fixed z-1 btn-dark-mode" onClick={toggleDarkMode}>
-            {darkMode ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />}
+            {darkMode ? <FontAwesomeIcon icon={faLightbulb} /> : <FontAwesomeIcon icon={faMoon} />}
         </button>
     )
 }
