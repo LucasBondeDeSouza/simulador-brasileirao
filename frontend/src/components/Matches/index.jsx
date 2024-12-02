01a4db30d0026ccf5a2d1f249a5246e4631ce6e8
Lucas_Bonde <111398229+LucasBondeDeSouza@users.noreply.github.com>
2024-12-02 11:07:28 -0300

    atualização

--- frontend/src/components/Matches/index.jsx
+++ frontend/src/components/Matches/index.jsx
@@ -3,7 +3,7 @@ import Round from "./Round";
 import Games from "./Games";
 
 export default ({ setHighlightedTeams, setScores, scores, updateTeams, removeScore, darkMode }) => {
-    const [round, setRound] = useState(36)
+    const [round, setRound] = useState(37)
 
     return (
         <div className="col-12 col-lg-5">
